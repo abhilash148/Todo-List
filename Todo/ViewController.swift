fe54310ad344fe5d0d3fd863a83b0e767db04a94
abhilash148 <saiabhilash148@gmail.com>
2022-04-17 11:22:15 +0530

    17-04-22: Todo UI with coredata

---
+++ Todo/ViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Sai Abhilash Gudavalli on 17/04/22.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var tasks: [NSManagedObject] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "TASKS"
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Todo")
+        
+        do {
+            tasks = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("unable to fetch: \(error)")
+        }
+    }
+    
+}
+
+// Actions for alert popup
+extension ViewController {
+
+    @IBAction func AddTask(_ sender: UIBarButtonItem) {
+        
+        let alertController = UIAlertController(title: "Task",
+                                      message: "Add a new task",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) { [unowned self] action in
+            guard let textField = alertController.textFields?.first, let task = textField.text else {
+                return
+            }
+            
+            self.saveTask(name: task)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel)
+        
+        alertController.addTextField()
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let task = tasks[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = task.value(forKey: "task") as? String
+        
+        return cell
+    }
+}
+
+// Save task to coredata
+extension ViewController {
+    
+    func saveTask(name: String) {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Todo", in: managedContext)!
+        
+        let task = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        task.setValue(name, forKey: "task")
+        
+        do {
+            try managedContext.save()
+            tasks.append(task)
+        } catch let error as NSError {
+            print("could not save: \(error)")
+        }
+    }
+}
